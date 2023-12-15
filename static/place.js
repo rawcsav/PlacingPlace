9452463ea4904d7eec598a11d53f1a9d9e055524
rawc <gmasontf2@gmail.com>
2023-12-15 07:17:46 -0500

    condensed info into one json tree, madeinfo div

--- static/place.js
+++ static/place.js
@@ -1,99 +1,235 @@
-var map = L.map("mapid").setView([20, 0], 2);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-var philosophers = {
+var philosopherTimelines = {
   Locke: {
     color: "blue",
-    places: [
+    timeline: [
       {
-        placeName: "Somerset, Bristol",
+        year: 1632,
+        placeName: "Wrington, Somerset",
+        description:
+          "Birth in Wrington, Somerset, amidst political tension leading to the English Civil War.",
         coords: [51.364334734083414, -2.7660054204208193]
       },
       {
-        placeName: "Westminster School and Christ Church, Oxford",
-        coords: [51.75158533501807, -1.2493966827927934]
+        year: 1652,
+        placeName: "Westminster School, London",
+        description: "Enters Westminster School, begins classical education.",
+        coords: [51.53609024297062, -0.2608172190546055]
       },
       {
+        year: 1659,
+        placeName: "Christ Church, Oxford",
+        description:
+          "Continues to Christ Church, Oxford, developing a critical view of scholasticism.",
+        coords: [51.75093669986004, -1.2568811610925572]
+      },
+      {
+        year: 1668,
         placeName: "The Royal Society, London",
+        description:
+          "Joins the Royal Society, engaging with the scientific community.",
         coords: [51.5060281634301, -0.1323867897652474]
       },
       {
+        year: 1679,
+        placeName: "Province of Carolina, North America",
+        description:
+          "Works in various governmental roles, including Secretary of the Board of Trade and Plantations and Secretary to the Lords Proprietors of Carolina, Through this work, gains practical insights into colonial administration.",
+        coords: [35.723709508383116, -79.5461781717278]
+      },
+      {
+        year: 1683,
+        description:
+          "Flees to Holland during political exile, refining his political philosophy.",
         placeName: "Amsterdam, Holland",
         coords: [52.364195355814836, 4.908067823689793]
+      },
+      {
+        year: 1689,
+        description:
+          'Publishes "Two Treatises of Government" after the Glorious Revolution, outlining his views on property and governance.',
+        placeName: "London, England",
+        coords: [51.5060281634301, -0.1323867897652474]
+      },
+      {
+        year: 1704,
+        description: "Passes away in High Laver, Essex.",
+        placeName: "High Laver, Essex",
+        coords: [51.75707029252346, 0.21150242994286808]
       }
     ]
   },
   Kant: {
     color: "red",
-    places: [
+    timeline: [
       {
-        placeName: "Home, Königsberg",
+        year: 1724,
+        description: "Birth in Königsberg, Prussia.",
+        placeName: "Königsberg, Prussia",
         coords: [54.59374872010948, 22.030330580055207]
       },
       {
+        year: 1740,
+        description: "Enrolls at the University of Königsberg.",
         placeName: "University of Königsberg",
         coords: [54.72444827308849, 20.528135056022908]
+      },
+      {
+        year: 1804,
+        description: "Death in Königsberg.",
+        placeName: "Königsberg, Prussia",
+        coords: [54.59374872010948, 22.030330580055207]
       }
     ]
   },
   Heidegger: {
     color: "green",
-    places: [
+    timeline: [
       {
+        year: 1889,
+        description: "Born in Meßkirch, Baden, Germany.",
         placeName: "Meßkirch Castle, Baden",
         coords: [47.9934459135849, 9.111475420764133]
       },
       {
+        year: 1909,
+        description: "Begins studies at the University of Freiburg.",
         placeName: "University of Freiburg",
         coords: [47.99548196726819, 7.848184459049453]
       },
       {
+        year: 1923,
+        description: "Appointed to the University of Marburg.",
         placeName: "Marburg",
         coords: [50.80109297194249, 8.76409702613222]
       },
       {
+        year: 1927,
+        description: "Publication of 'Being and Time'.",
         placeName: "Todtnauberg, Germany",
         coords: [47.85129621625994, 7.939863923607821]
+      },
+      {
+        year: 1933,
+        description:
+          "Becomes rector of the University of Freiburg; joins the Nazi Party.",
+        placeName: "University of Freiburg",
+        coords: [47.99548196726819, 7.848184459049453]
+      },
+      {
+        year: 1934,
+        description: "Resigns as rector and faces political controversy.",
+        placeName: "University of Freiburg",
+        coords: [47.99548196726819, 7.848184459049453]
+      },
+      {
+        year: 1951,
+        description: "Delivers 'Building Dwelling Thinking' lecture.",
+        placeName: "University of Freiburg",
+        coords: [47.99548196726819, 7.848184459049453]
+      },
+      {
+        year: 1976,
+        description: "Dies in Freiburg, Germany.",
+        placeName: "University of Freiburg",
+        coords: [47.99548196726819, 7.848184459049453]
       }
     ]
   },
   Descartes: {
     color: "orange",
-    places: [
+    timeline: [
       {
+        year: 1596,
+        description: "Born in La Haye en Touraine, France.",
         placeName: "La Haye en Touraine",
         coords: [46.974392031935515, 0.6980432371616763]
       },
       {
+        year: 1606,
+        description:
+          "Attends Jesuit Collège Royal Henry-Le-Grand in La Flèche.",
         placeName: "Jesuit Collège Royal Henry-Le-Grand in La Flèche",
         coords: [47.70078956424511, -0.07615513640528236]
       },
       {
+        year: 1618,
+        description: "Military service, traveling through Europe.",
+        placeName: "Battle of White Mountain, Bohemia",
+        coords: [44.164841161477405, -71.43084073895686]
+      },
+      {
+        year: 1619,
+        description: "Experiences transformative dreams in Neuburg.",
+        placeName: "Neuburg an der Donau",
+        coords: [48.73249975579165, 11.188758005854991]
+      },
+      {
+        year: 1628,
+        description:
+          "Moves to the Dutch Republic, engaging in scientific work.",
         placeName: "University of Franeker, Dutch Republic",
         coords: [53.218044568204945, 5.5084435901227184]
       },
       {
-        placeName: "Neuburg an der Donau",
-        coords: [48.73249975579165, 11.188758005854991]
+        year: 1637,
+        description: "Publishes 'Discourse on the Method'.",
+        placeName: "University of Franeker, Dutch Republic",
+        coords: [53.218044568204945, 5.5084435901227184]
+      },
+      {
+        year: 1641,
+        description: "Publishes 'Meditations on First Philosophy'.",
+        placeName: "University of Utrecht, Dutch Republic",
+        coords: [52.090908653342765, 5.119567496166806]
+      },
+      {
+        year: 1649,
+        description: "Invited to Sweden to tutor Queen Christina.",
+        placeName: "Stockholm, Sweden",
+        coords: [59.331657947917236, 18.053231081396415]
+      },
+      {
+        year: 1650,
+        description: "Dies in Stockholm, Sweden.",
+        placeName: "Stockholm, Sweden",
+        coords: [59.331657947917236, 18.053231081396415]
       }
     ]
   },
   Cronon: {
     color: "purple",
-    places: [
+    timeline: [
       {
+        year: 1954,
+        description:
+          "Born in Connecticut, beginning of environmental awareness.",
         placeName: "Yale University, CT",
         coords: [41.316903030981976, -72.92262398049343]
       },
       {
+        year: 1976,
+        description:
+          "Graduates from Yale University, interdisciplinary studies shape perspective.",
+        placeName: "Yale University, CT",
+        coords: [41.316903030981976, -72.92262398049343]
+      },
+      {
+        year: 1981,
+        description:
+          "Earns doctorate from Oxford, further developing academic approach to 'place'.",
         placeName: "University of Oxford, England",
         coords: [51.75492263493429, -1.2536694288066255]
       },
       {
+        year: 1992,
+        description:
+          "Publication of 'Nature's Metropolis: Chicago and the Great West'.",
+        placeName: "University of Wisconsin-Madison",
+        coords: [43.07682707425444, -89.41201543479144]
+      },
+      {
+        year: 2023,
+        description: "Continues his work to the present day.",
         placeName: "University of Wisconsin-Madison",
         coords: [43.07682707425444, -89.41201543479144]
       }
@@ -101,30 +237,209 @@ var philosophers = {
   },
   Mignolo: {
     color: "yellow",
-    places: [
+    timeline: [
       {
+        year: 1941,
+        description:
+          "Born in Argentina, amidst postcolonial context of Latin America.",
         placeName: "Corral de Bustos, Argentina",
         coords: [-33.2843037078034, -62.185690534741674]
       },
       {
+        year: 1980,
+        description:
+          "Development of key concepts such as the colonial difference and border thinking.",
         placeName: "École des Hautes Études, Paris",
         coords: [48.85636103600829, 2.3618558343322276]
+      },
+      {
+        year: 2000,
+        description:
+          "Work gains international recognition, influencing debates on coloniality and geopolitics of knowledge.",
+        placeName:
+          "John Hope Franklin Center for Interdisciplinary and International Studies, Duke",
+        coords: [36.00832522947684, -78.93368842956822]
+      },
+      {
+        year: 2023,
+        description: "Continues his work to the present day.",
+        placeName: "ÉPolitecnica Salesiana University, Ecuador",
+        coords: [-2.886368368719161, -78.98951432410801]
       }
     ]
   }
 };
-for (var philosopher in philosophers) {
-  var locations = philosophers[philosopher].coords;
-  var color = philosophers[philosopher].color;
+var map = L.map("mapid").setView([20, 0], 2);
 
-  locations.forEach(function (location) {
-    var marker = L.circleMarker(location, {
-      color: color,
-      fillColor: color,
-      fillOpacity: 0.5,
-      radius: 8
-    }).addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-    marker.bindPopup(philosopher);
+function addMarkersToMap(philosopherTimelines, mapInstance) {
+  var legend = L.control({ position: "bottomright" });
+
+  legend.onAdd = function (mapInstance) {
+    var div = L.DomUtil.create("div", "info legend");
+    var labels = [];
+    var categories = Object.keys(philosopherTimelines);
+
+    categories.forEach(function (philosopher) {
+      labels.push(
+        '<div class="phil-legend"><i style="background:' +
+          philosopherTimelines[philosopher].color +
+          '; width: 18px; height: 18px; float: left; margin-right: 8px; opacity: 0.7;"></i>' +
+          philosopher +
+          "</div>"
+      );
+    });
+
+    div.innerHTML = labels.join("<br>");
+    return div;
+  };
+
+  legend.addTo(mapInstance);
+
+  Object.keys(philosopherTimelines).forEach(function (philosopher) {
+    philosopherTimelines[philosopher].timeline.forEach(function (event) {
+      var marker = L.circleMarker(event.coords, {
+        color: philosopherTimelines[philosopher].color,
+        fillColor: philosopherTimelines[philosopher].color,
+        fillOpacity: 0.5,
+        radius: 8
+      }).addTo(mapInstance);
+
+      marker.bindPopup(
+        "<strong>" + event.placeName + "</strong>: " + event.description
+      );
+    });
   });
 }
+
+addMarkersToMap(philosopherTimelines, map);
+
+function createTimeline(philosopherTimelines) {
+  d3.select("#timeline svg").remove();
+
+  var currentYear = new Date().getFullYear();
+
+  var allEvents = [];
+  Object.keys(philosopherTimelines).forEach(function (key) {
+    philosopherTimelines[key].timeline.forEach(function (event) {
+      allEvents.push({
+        philosopher: key,
+        year: event.year,
+        description: event.description,
+        coords: event.coords
+      });
+    });
+  });
+
+  var yearExtent = d3.extent(allEvents, function (d) {
+    return d.year;
+  });
+  yearExtent[1] = Math.max(yearExtent[1], currentYear);
+  var margin = { top: 20, right: 30, bottom: 30, left: 60 },
+    width =
+      parseInt(d3.select("#timeline").style("width"), 10) -
+      margin.left -
+      margin.right,
+    height = 250 - margin.top - margin.bottom;
+
+  var svg = d3
+    .select("#timeline")
+    .append("svg")
+    .attr("width", "100%")
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var x = d3.scaleLinear().domain(yearExtent).range([0, width]);
+  svg
+    .append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+
+  var y = d3
+    .scaleBand()
+    .domain(Object.keys(philosopherTimelines))
+    .range([0, height])
+    .padding(0.1);
+  svg.append("g").call(d3.axisLeft(y));
+
+  var tooltip = d3
+    .select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+  svg
+    .selectAll(".life-span")
+    .data(Object.entries(philosopherTimelines))
+    .enter()
+    .append("line")
+    .attr("class", "life-span")
+    .attr("x1", function (d) {
+      return x(
+        d3.min(d[1].timeline, function (t) {
+          return t.year;
+        })
+      );
+    })
+    .attr("x2", function (d) {
+      return x(
+        d3.max(d[1].timeline, function (t) {
+          return t.year;
+        })
+      );
+    })
+    .attr("y1", function (d) {
+      return y(d[0]) + y.bandwidth() / 2;
+    })
+    .attr("y2", function (d) {
+      return y(d[0]) + y.bandwidth() / 2;
+    })
+    .attr("stroke", function (d) {
+      return philosopherTimelines[d[0]].color;
+    })
+    .attr("stroke-width", 4);
+
+  svg
+    .selectAll(".event-dot")
+    .data(allEvents)
+    .enter()
+    .append("circle")
+    .attr("class", "event-dot")
+    .attr("cx", function (d) {
+      return x(d.year);
+    })
+    .attr("cy", function (d) {
+      return y(d.philosopher) + y.bandwidth() / 2;
+    })
+    .attr("r", 5)
+    .attr("fill", function (d) {
+      return philosopherTimelines[d.philosopher].color;
+    })
+    .on("mouseover", function (event, d) {
+      tooltip.transition().duration(200).style("opacity", 0.9);
+      tooltip
+        .html("<strong>" + d.philosopher + "</strong><br>" + d.description)
+        .style("left", event.pageX + "px")
+        .style("top", event.pageY - 28 + "px");
+    })
+    .on("mouseout", function (d) {
+      tooltip.transition().duration(500).style("opacity", 0);
+    })
+    .on("click", function (event, d) {
+      if (d.coords) {
+        map.flyTo(d.coords, 13, {
+          animate: true,
+          duration: 1.5
+        });
+      }
+    });
+}
+
+window.onload = window.onresize = function () {
+  createTimeline(philosopherTimelines);
+};
